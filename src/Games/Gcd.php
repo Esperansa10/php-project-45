eb613ad23d58e82ef15f1987daa8acb98e1b9783
Nadin <esperansa10@gmail.com>
2023-10-24 11:52:36 +0300

    change progression game

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -2,41 +2,31 @@
 
 namespace BrainGames\Gcd;
 
-use BrainGames\Cli;
 use BrainGames\Engine;
-use function cli\greet;
 use function cli\line;
 use function cli\prompt;
-use function Engine\engine;
-use function gmp_gcd;
-
 
 function gcd()
 {
-
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-
-
     line('Find the greatest common divisor of given numbers.');
 
     for ($i = 1; $i <= 3; $i++) {
 
         $result = '';
         $a = rand(1, 100);
-        $b =  rand(1, 100);
+        $b = rand(1, 100);
 
         line('Question: ' . $a . ' ' . $b);
         $answer = prompt('Your answer');
         $answer = intval($answer);
 
         //считаем result 
-
         $result = gcd_result($a, $b);
 
         // закончили считать result
-
         //сравниваем result и answer   
         if ($answer === $result) {
             Engine\checkAnser($answer, $result,  $name);
