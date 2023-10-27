2638e03cf9aae7ee86b48383cb7ea2429542437f
Nadin <esperansa10@gmail.com>
2023-10-27 16:53:32 +0300

    fix even game

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -9,45 +9,64 @@ use function cli\prompt;
 
 function even()
 {
-
-
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 
     line('Answer "yes" if the number is even, otherwise answer "no".');
+
     $random = '';
-    $result = '';
+
 
     for ($i = 1; $i <= 3; $i++) {
         line('Question: ' . $random = rand(1, 10));
         $answer = prompt('Your answer');
 
-        if ($random % 2 === 1 && $answer === 'yes') {
+        $result = primeCheck($random);
+
+        if ($result === 0 && $answer === 'yes') {
             $result = 'no';
             $answer = 'yes';
             return Engine\checkAnser($answer, $result, $name);
-        } elseif ($random % 2 === 0 & $answer === 'no') {
+        } elseif ($result === 1 && $answer === 'no') {
             $result = 'yes';
             $answer = 'no';
+
             return  Engine\checkAnser($answer, $result, $name);
-        } elseif ($random % 2 === 1 & $answer !== 'no') {
-            $result = 'yes';
-            $answer = 'no';
-            return  Engine\checkAnser($answer, $result, $name);
-        } elseif ($random % 2 === 0 & $answer !== 'yes') {
+        } elseif ($result === 0 && $answer !== 'no') {
             $result = 'no';
             $answer = 'yes';
             return  Engine\checkAnser($answer, $result, $name);
-        } elseif ($random % 2 === 0 & $answer === 'yes') {
+        } elseif ($result === 1 && $answer !== 'yes') {
+            $result = 'yes';
+            $answer = 'no';
+            return  Engine\checkAnser($answer, $result, $name);
+        } elseif ($result === 1 && $answer === 'yes') {
             $result = 'yes';
             $answer = 'yes';
             Engine\checkAnser($answer, $result, $name);
-        } elseif ($random % 2 === 1 & $answer === 'no') {
+        } elseif ($result === 0 && $answer === 'no') {
             $result = 'no';
             $answer = 'no';
             Engine\checkAnser($answer, $result, $name);
         }
     }
     return line("Congratulations, " .  $name . "!");
+}
+
+//cчитаем, простое ли число
+// 1 простое число
+// 0 непростое число
+function primeCheck(int $number)
+{
+    if ($number == 1) {
+        return 0;
+    }
+
+    for ($i = 2; $i <= $number / 2; $i++) {
+        if ($number % $i == 0) {
+            return 0;
+        }
+    }
+    return 1;
 }
