7d8722d6ed4ba197b057f9d82120aabf025ff4f7
Nadin <esperansa10@gmail.com>
2023-11-05 12:12:40 +0300

    some correts

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -9,45 +9,45 @@ use function cli\prompt;
 
 function progression()
 {
-  line('Welcome to the Brain Games!');
-  $name = prompt('May I have your name?');
-  line("Hello, %s!", $name);
-  line('What number is missing in the progression?');
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('What number is missing in the progression?');
 
-  for ($i = 1; $i <= 3; $i++) {
-    // создаем стартовое число массива
-    $counter = 0; //счетчик заполнения элементов массива
-    $stepOfProgression =  rand(1, 5); // шаг между числами
-    $counter = $counter + $stepOfProgression; // к счетчику добавляем шаг, получаем стартовое число с которого начнется массив
-    $arrOfProgression = []; // массив прогрессии
+    for ($i = 1; $i <= 3; $i++) {
+      // создаем стартовое число массива
+        $counter = 0; //счетчик заполнения элементов массива
+        $stepOfProgression =  rand(1, 5); // шаг между числами
+        $counter = $counter + $stepOfProgression; // к счетчику добавляем шаг, получаем стартовое число с которого начнется массив
+        $arrOfProgression = []; // массив прогрессии
 
-    //считаем массив
-    for ($lenghtOfArrOfProgression = 1; $lenghtOfArrOfProgression <= 10; $lenghtOfArrOfProgression++) { //задаем длину массива, название жесть конечно, зато понятно
-      $counter = $counter + $stepOfProgression; //создаем стартовое число массива 
-      $arrOfProgression[] = $counter + $stepOfProgression; // заполняем числами массив
-    };
-    //закончили считать массив
+      //считаем массив
+        for ($lenghtOfArrOfProgression = 1; $lenghtOfArrOfProgression <= 10; $lenghtOfArrOfProgression++) { //задаем длину массива, название жесть конечно, зато понятно
+            $counter = $counter + $stepOfProgression; //создаем стартовое число массива
+            $arrOfProgression[] = $counter + $stepOfProgression; // заполняем числами массив
+        };
+      //закончили считать массив
 
-    $result = '';
+        $result = '';
 
 
-    //ставим пробел в массиве
-    $indexchange = rand(0, 9);  //хаотично выбираем где будет пробел
-    $result = $arrOfProgression[$indexchange]; // записываем его как индекс массива + как верный результат
-    $arrOfProgression[$indexchange] = '..'; // пробуем сразу ставить 2 точки
-    $question = implode(' ', $arrOfProgression);
+      //ставим пробел в массиве
+        $indexchange = rand(0, 9);  //хаотично выбираем где будет пробел
+        $result = $arrOfProgression[$indexchange]; // записываем его как индекс массива + как верный результат
+        $arrOfProgression[$indexchange] = '..'; // пробуем сразу ставить 2 точки
+        $question = implode(' ', $arrOfProgression);
 
-    //закончили ставить пробел в массиве
-    line('Question: ' . $question);
-    $answer = prompt('Your answer');
-    $answer = intval($answer);
+      //закончили ставить пробел в массиве
+        line('Question: ' . $question);
+        $answer = prompt('Your answer');
+        $answer = intval($answer);
 
-    //сравниваем result и answer
-    if ($answer === $result) {
-      Engine\checkAnser($answer, $result, $name);
-    } else {
-      return Engine\checkAnser($answer, $result, $name);
+      //сравниваем result и answer
+        if ($answer === $result) {
+            Engine\checkAnser($answer, $result, $name);
+        } else {
+            return Engine\checkAnser($answer, $result, $name);
+        }
     }
-  }
-  line("Congratulations, " . $name . "!");
+    line("Congratulations, " . $name . "!");
 }
